6c1a3d4f36f4b54cc9bbdff3009be47e55547a09
Manuel Lorenzo <manulorenzop@gmail.com>
2023-01-21 12:54:31 +0100

    configure RestTemplate to have a timeout

--- src/main/java/com/nulltwenty/ordersaggregation/OrdersAggregationApplication.java
+++ src/main/java/com/nulltwenty/ordersaggregation/OrdersAggregationApplication.java
@@ -5,17 +5,21 @@ import com.nulltwenty.ordersaggregation.serializer.TrackResponseSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrdersAggregationApplication {
+    private final int DEFAULT_TIMEOUT = 5000;
+
     public static void main(String[] args) {
         SpringApplication.run(OrdersAggregationApplication.class, args);
     }
 
     @Bean
     RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(clientHttpRequestFactory());
     }
 
     @Bean
@@ -26,5 +30,12 @@ public class OrdersAggregationApplication {
     @Bean
     AggregatedResponseSerializer aggregatedResponseSerializer() {
         return new AggregatedResponseSerializer();
+    }
+
+    @Bean
+    ClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(DEFAULT_TIMEOUT);
+        return clientHttpRequestFactory;
     }
 }
